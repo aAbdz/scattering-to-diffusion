d2a63d8ba73ee6231532e1add6d5996957ba8cb3
aAbdz <aliabd@LT1PFG4317.local>
2025-07-29 21:22:18 +0300

    init

--- util_gamma0_decomposition.m
+++ util_gamma0_decomposition.m
@@ -12,10 +12,8 @@ filt_area = smoothdata(area_i, 'gaussian', 20);
 filt_area(filt_area<min(area_i)) = min(area_i);
 a0 = min(filt_area);
 
-e0 = log(a0 / mean(filt_area));
-filt_eta(filt_eta<=e0) = e0;
 
-min_d = 2/dx; % minimum of 2um dist between beads
+min_d = 2/dx;   % minimum of 2um dist between beads
 min_w = 0.5/dx; % minimum of 0.5um bead width
 [~, loc_mins] = findpeaks(-filt_area, 'MinPeakDistance', min_d, 'MinPeakWidth', min_w); 
 loc_mins = [1, loc_mins, N];
@@ -42,6 +40,7 @@ loc_maxs = loc_maxs*dx;
 abar = mean(diff(loc_maxs));
 astd = std(diff(loc_maxs));
 
+e0 = min(filt_eta);
 bead_v = sum(filt_eta - e0) * dx;
 av_bead_v = bead_v / abar;
 
